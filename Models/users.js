56e716e7f7413bea8023d3f41a3edb89427d1ff3
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-04-20 11:20:02 +0200

    created users.js

--- Models/users.js
+++ Models/users.js
@@ -1,2 +1,10 @@
 const mongodb = require('mongoose');
 
+const userstable = new mongoose.schema({
+    name : {type: string, required: true},
+    password : {type: string, required: true},
+    email: {type: string, required: true}
+} ,{timestamps : true})
+
+module.exports = mongoose.models('users', userstable);
+
