d722eb772c60b54a9330e834e90ad3ea93927f95
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-04-20 11:22:37 +0200

    beautified everything

--- Models/notes.js
+++ Models/notes.js
@@ -4,7 +4,7 @@ const notestable = new mongoose.Schema({
     title : { type: string, required: true},
     content: { type: string, required: true},
     date: {type: number, required: true},
-
+    notifyme : {type : number, required: false}
 },{timestamps : true});
 
 module.exports = mongoose.model('notes', notestable);
