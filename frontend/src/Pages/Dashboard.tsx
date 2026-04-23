c06fa1856f34ed944502259fc03ba3c463a295fd
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-04-23 13:39:19 +0200

    included also icosn

--- frontend/src/Pages/Dashboard.tsx
+++ frontend/src/Pages/Dashboard.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { HStack, Grid, GridItem, Box , Flex} from "@chakra-ui/react";
+import { MdAdd, MdEdit, MdDelete } from "react-icons/md";
+import { HStack, Grid, GridItem, Box, Flex } from "@chakra-ui/react";
 const Dashboard = () => {
   return (
     <>
@@ -14,42 +14,76 @@ const Dashboard = () => {
         fontFamily="serif"
         fontSize={28}
         fontWeight="bold"
-        width= "1000"
-      >Todolist--Dashboard</HStack>
+        width="1000"
+      >
+        Todolist--Dashboard
+      </HStack>
 
-    <Grid
-    templateColumns={{base: "1fr", md: "1fr 1fr"}}
-    width= "1000">
+      <Grid templateColumns={{ base: "1fr", md: "1fr 1fr" }} width="1000">
         <GridItem
-        border={1}
-        borderColor={'white'}
-        borderStyle={'solid'}
-        backgroundImage="url('/bg.jpg')"
-        backgroundSize="cover"
-        backgroundRepeat ="no-repeat"
-        height="50vw">
-            <Flex
-            flexDirection={'column'}
+          border={1}
+          borderColor={"white"}
+          borderStyle={"solid"}
+          // backgroundImage="url('/bg.jpg')"
+          backgroundSize="cover"
+          backgroundRepeat="no-repeat"
+        >
+          <Flex
+            flexDirection={"column"}
             gap={20}
-            align='center'
-            justify='center'
-            marginTop={10}>
-             <Box
-            border={1} height={150} width={250} borderColor={"white"} borderStyle={"solid"}></Box>   
-             <Box
-            border={1} height={150} width={250} borderColor={"white"} borderStyle={"solid"}></Box>   
-             <Box
-            border={1} height={150} width={250} borderColor={"white"} borderStyle={"solid"}></Box>   
-            </Flex>
-            
+            align="center"
+            justify="center"
+            marginTop={10}
+          >
+            <Box
+              border={1}
+              height={150}
+              width={250}
+              borderColor={"white"}
+              borderStyle={"solid"}
+              borderRadius={10}
+              padding={5}
+            >
+              <HStack>
+                <MdAdd />
+                <Box>Create Note</Box>
+              </HStack>
+            </Box>
+            <Box
+              border={1}
+              height={150}
+              width={250}
+              borderColor={"white"}
+              borderStyle={"solid"}
+              borderRadius={10}
+              padding={5}
+            >
+                <HStack>
+                <MdEdit />
+                <Box>Update Note</Box>
+              </HStack>
+            </Box>
+            <Box
+              border={1}
+              height={150}
+              width={250}
+              borderColor={"white"}
+              borderStyle={"solid"}
+              marginBottom={5}
+              borderRadius={10}
+              padding={5}
+            >
+                <HStack>
+                <MdDelete />
+                <Box>Delete Note</Box>
+              </HStack>
+            </Box>
+          </Flex>
         </GridItem>
-        <GridItem
-        border={1}
-        borderColor={'white'}
-        borderStyle={'solid'}>
-            this is grid two
+        <GridItem border={1} borderColor={"white"} borderStyle={"solid"}>
+          this is grid two
         </GridItem>
-    </Grid>
+      </Grid>
     </>
   );
 };
