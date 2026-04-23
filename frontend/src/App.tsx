b65789dda1edf860ee5f7c63ef710fcdd9fcb84a
blaisekwizera31-hash <blaisekwizera31@gmail.com>
2026-04-23 16:19:38 +0200

    added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
-
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom'
+import Signup from './Pages/signup'
+import Login from './Pages/login'
 import Dashboard from './Pages/Dashboard'
 
 const App = () => {
   return (
-    <Dashboard />
+   <Router>
+    <Routes>
+      <Route path='/' element={<Login/>} />
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
+      </Routes>
+   </Router>
   )
 }
 
